9634208212aaeb7e16e5ae056ab0da1e2d931bd3
alvaro <alvarodevcode@outlook.com>
2023-05-05 10:32:56 -0500

    fix: deployment fix

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,6 @@ import NextAuth from "next-auth";
 
 import { authOptions } from "~/server/auth";
 
-const handler = NextAuth(authOptions) as unknown;
+const handler = NextAuth(authOptions) as VoidFunction;
 
 export { handler as POST, handler as GET };
