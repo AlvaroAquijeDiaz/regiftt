cc1a50016a00246ededde2148a647bf908b7cdcd
alvaro <alvarodevcode@outlook.com>
2023-05-08 13:18:27 -0500

    chore: redeploy?

--- src/app/(authenticated)/my-wishes/page.tsx
+++ src/app/(authenticated)/my-wishes/page.tsx
@@ -1,9 +1,6 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "~/server/auth";
 import { NewWishForm } from "~/ui/my-wishes/new-wish-form";
 
-export default async function MyWishesPage() {
-  const session = await getServerSession(authOptions);
+export default function MyWishesPage() {
   return (
     <>
       <h1 className="text-3xl font-bold">My Wishes</h1>
