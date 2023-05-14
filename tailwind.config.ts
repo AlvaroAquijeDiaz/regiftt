9a188ff04515efbd38e0e22197c1add69b87483c
alvaro <alvarodevcode@outlook.com>
2023-05-13 23:35:55 -0500

    style: updates

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -70,6 +70,7 @@ export default {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "radix-dialog-out": "closed-dialog 0.2s ease-out",
       },
     },
   },
