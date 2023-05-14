027dba0939ff5b46eddc3b64dde299b8b389c907
alvaro <alvarodevcode@outlook.com>
2023-05-14 00:07:04 -0500

    fix: deplooy fix

--- src/ui/my-wishes/my-wishes.schemas.ts
+++ src/ui/my-wishes/my-wishes.schemas.ts
@@ -1,24 +1,20 @@
 import { z } from "zod";
 
-export const NewWishSchema = z
-  .object({
-    name: z.string().min(1).max(100),
-    description: z.string().max(1000).optional(),
-    url: z.string().optional(),
-    priceKnown: z.boolean().optional().default(false),
-    price: z
-      .number({
-        // TODO: Continue refining
-        invalid_type_error: "Please enter a valid number",
-      })
-      .positive()
-      .max(10000000, {
-        message: "Mmm 10M for a gift is a bit too much, don't you think?",
-      })
-      .optional(),
-  })
-  .refine((data) => data.priceKnown === false && (data.price === undefined || isNaN(data.price)), {
-    message: "Please enter a price if you know an estimate",
-  });
+export const NewWishSchema = z.object({
+  name: z.string().min(1).max(100),
+  description: z.string().max(1000).optional(),
+  url: z.string().optional(),
+  priceKnown: z.boolean().optional().default(false),
+  price: z
+    .number({
+      // TODO: Continue refining
+      invalid_type_error: "Please enter a valid number",
+    })
+    .positive()
+    .max(10000000, {
+      message: "Mmm 10M for a gift is a bit too much, don't you think?",
+    })
+    .optional(),
+});
 
 export type NewWishSchema = z.infer<typeof NewWishSchema>;
