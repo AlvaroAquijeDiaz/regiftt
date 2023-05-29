a0ae1c2f3ce0279f1deaf31bc09c500c6e9a0881
alvaro <alvarodevcode@outlook.com>
2023-05-28 23:19:54 -0500

    feat: ui updates

--- src/providers/next-auth.tsx
+++ src/providers/next-auth.tsx
@@ -1,7 +1,7 @@
-'use client';
+"use client";
 
-import { SessionProvider } from 'next-auth/react';
-import { type FunctionComponent, type ReactNode } from 'react';
+import { SessionProvider } from "next-auth/react";
+import { type FunctionComponent, type ReactNode } from "react";
 
 export const NextAuthProvider: FunctionComponent<{ children: ReactNode }> = ({ children }) => {
   return <SessionProvider>{children}</SessionProvider>;
--- src/ui/home/sidebar.tsx
+++ src/ui/home/sidebar.tsx
@@ -26,7 +26,7 @@ export const segments = [
 
 export const Sidebar = () => {
   return (
-    <aside className="relative left-0 top-0 z-20 flex h-screen w-[300px] flex-col gap-3.5 border-r bg-neutral-100/70 pb-3 pt-3 text-sm transition-transform duration-75 max-lg:fixed max-lg:-translate-x-full lg:sticky lg:min-w-[260px] lg:pt-2">
+    <aside className="relative left-0 top-0 z-20 flex max-h-screen w-[300px] flex-col gap-3.5 overflow-y-hidden border-r bg-neutral-100/70 pb-3 pt-3 text-sm transition-transform duration-75 max-lg:fixed max-lg:-translate-x-full lg:sticky lg:min-w-[260px] lg:pt-2">
       <div className="mx-4 flex select-none items-center justify-between gap-1 rounded-lg border bg-white px-2 py-1">
         <Suspense fallback={<Spinner />}>
           {/* @ts-expect-error RSC */}
