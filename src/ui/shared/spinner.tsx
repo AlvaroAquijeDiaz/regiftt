5d227f77591916156105e8b99b2a5ed44efc51ce
alvaro <alvarodevcode@outlook.com>
2023-06-05 23:02:24 -0500

    fix: removed prev files

--- src/ui/shared/spinner.tsx
+++
@@ -1,7 +0,0 @@
-import { Loader } from "lucide-react";
-
-export const Spinner = () => {
-  return (
-    <Loader className="flex animate-bounce animate-spin items-center justify-center text-neutral-400" />
-  );
-};
